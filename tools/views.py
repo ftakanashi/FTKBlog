f56d89749c28313009ba5b9e263b681e9c2e3761
unknown <wyzypa@gmail.com>
2019-03-17 20:43:48 +0800

    v1.7.3 修改了人体生物节律查询界面的日期输入方式

--- tools/views.py
+++ tools/views.py
@@ -7,11 +7,13 @@ from django.conf import settings
 from django.http import JsonResponse
 from ratelimit.decorators import ratelimit
 
+import datetime
 import logging
 import paramiko
 import traceback
 
 logger = logging.getLogger('django')
+
 
 # Create your views here.
 
@@ -36,6 +38,7 @@ def get_proxy_ssh_client():
     else:
         return ssh
 
+
 def close_proxy_ssh_client(ssh):
     try:
         ssh.close()
@@ -44,16 +47,20 @@ def close_proxy_ssh_client(ssh):
     else:
         return True
 
-class BiologyRhythmView(View):
 
+class BiologyRhythmView(View):
     @ratelimit(key='ip', rate='1/1s')
     def get(self, request):
-
         if getattr(request, 'limited', False):
             return render(request, 'error.html', {'error_msg': '你点得太急了 稍微过一会儿再试吧Σ(っ °Д °;)っ', 'error_title': ''})
 
         ctx = {}
+        ctx['year_opt'] = range(1970, 2031)
+        ctx['month_opt'] = range(1,13)
+        ctx['day_opt'] = range(1,32)
+        ctx['today'] = datetime.datetime.today()
         return render(request, 'tools/biorhy.html', ctx)
+
 
 class PNHBDownloadView(View):
     @ratelimit(key='ip', rate='1/1s')
@@ -74,17 +81,18 @@ class PNHBDownloadView(View):
         try:
             script = settings.TOOLS_CONFIG['pnhb']['script_path']
         except Exception as e:
+            logger.error(traceback.format_exc(e))
             return JsonResponse({'msg': '获取远程工作节点工作脚本目录失败'}, status=500)
 
         ssh = get_proxy_ssh_client()
         if ssh is None:
-            return JsonResponse({'msg': '获取远程工作节点客户端失败┭┮﹏┭┮'},status=500)
+            return JsonResponse({'msg': '获取远程工作节点客户端失败┭┮﹏┭┮'}, status=500)
 
         cmd = '{} -url {}'.format(script, url)
         if filename != '':
             cmd += ' -filename {}'.format(filename)
-        stdin,stdout,stderr = ssh.exec_command(cmd)
-        out,err = stdout.read(), stderr.read()
+        stdin, stdout, stderr = ssh.exec_command(cmd)
+        out, err = stdout.read(), stderr.read()
         close_proxy_ssh_client(ssh)
         if err or out.strip() != '0':
             logger.warning('下载节点远程stderr: {}'.format(err))
@@ -92,6 +100,7 @@ class PNHBDownloadView(View):
             return JsonResponse({'msg': '远程下载节点报错了┭┮﹏┭┮'})
         if out.strip() == '0':
             return JsonResponse({'msg': '下载命令已经顺利发出ヾ(◍°∇°◍)ﾉﾞ'})
+
 
 class SSRConfigView(View):
     @ratelimit(key='ip', rate='1/1s')
@@ -112,7 +121,7 @@ class SSRConfigView(View):
             return render(request, 'error.html', {'error_msg': '获取远程工作节点客户端失败┭┮﹏┭┮'})
 
         stdin, stdout, stderr = ssh.exec_command(show_port)
-        out,err = stdout.read(), stderr.read()
+        out, err = stdout.read(), stderr.read()
         close_proxy_ssh_client(ssh)
         if err:
             logger.error('获取远程SSR端口失败:\n{}'.format(err))
@@ -127,8 +136,9 @@ class SSRConfigView(View):
         port = request.POST.get('port')
 
         try:
-            config = settings.TOOLS_CONFIG['ssr_config']['config_path']
+            config = settings.TOOLS_CONFIG['ssr_config']['do_config_path']
         except Exception as e:
+            logger.error(traceback.format_exc(e))
             return JsonResponse({'msg': '获取远程工作节点脚本目录失败'}, status=500)
 
         cmd = config
@@ -137,7 +147,7 @@ class SSRConfigView(View):
 
         ssh = get_proxy_ssh_client()
         stdin, stdout, stderr = ssh.exec_command(cmd)
-        out,err = stdout.read(), stderr.read()
+        out, err = stdout.read(), stderr.read()
         close_proxy_ssh_client(ssh)
 
         if err:
